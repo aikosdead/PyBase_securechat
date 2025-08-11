ac995ede5754ee0a8f4ce0b394ef17847f393efa
Marc Dillera <marcdillera20@gmail.com>
2025-08-12 05:15:18 +0800

    firestore migration nga

--- app.py
+++ app.py
@@ -2,6 +2,19 @@
 
 from flask import Flask
 from app.routes.auth import auth_bp
+import firebase_admin
+from firebase_admin import credentials
+import os
+
+cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.environ.get("firebase-adminsdk-fbsvc@chat-52691.iam.gserviceaccount.com"),
+    "token_uri": "https://oauth2.googleapis.com/token"
+})
+
+firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
 app.register_blueprint(auth_bp)
